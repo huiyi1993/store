256d52b509be06fc12658e55677fbb16eb686ed9
huiyi <178175002@qq.com>
2021-04-21 15:06:03 +0800

    第一次提交回忆商城.py

---
+++ 回忆商城.py
@@ -0,0 +1,228 @@
+# 一号服装
+time1 = "1号"
+name1 = "羽绒服"
+pri1 = 253.6
+nmb1 = 500
+sal1 = 10
+# 二号服装
+time2 = "2号"
+name2 = "牛仔裤"
+pri2 = 86.3
+nmb2 = 600
+sal2 = 60
+# 三号服装
+time3 = "3号"
+name3 = "风衣"
+pri3 = 96.8
+nmb3 = 335
+sal3 = 43
+# 四号服装
+time4 = "4号"
+name4 = "皮草"
+pri4 = 135.9
+nmb4 = 855
+sal4 = 63
+# 五号服装
+time5 = "5号"
+name5 = "T恤"
+pri5 = 65.8
+nmb5 = 632
+sal5 = 63
+# 六号服装
+time6 = "6号"
+name6 = "衬衫"
+pri6 = 49.3
+nmb6 = 562
+sal6 = 120
+# 七号服装
+time7 = "7号"
+name7 = "牛仔裤"
+pri7 = 86.3
+nmb7 = 600
+sal7 = 72
+# 八号服装
+time8 = "8号"
+name8 = "羽绒服"
+pri8 = 253.6
+nmb8 = 500
+sal8 = 69
+# 九号服装
+time9 = "9号"
+name9 = "牛仔裤"
+pri9 = 86.3
+nmb9 = 600
+sal9 = 35
+# 十号服装
+time10 = "10号"
+name10 = "羽绒服"
+pri10 = 253.6
+nmb10 = 500
+sal10 = 140
+# 十一号服装
+time11 = "11号"
+name11 = "牛仔裤"
+pri11 = 86.3
+nmb11 = 600
+sal11 = 90
+# 十二号服装
+time12 = "12号"
+name12 = "皮草"
+pri12 = 135.9
+nmb12 = 855
+sal12 = 24
+# 十三号服装
+time13 = "13号"
+name13 = "T恤"
+pri13 = 65.8
+nmb13 = 632
+sal13 = 45
+# 十四号服装
+time14 = "14号"
+name14 = "风衣"
+pri14 = 96.8
+nmb14 = 355
+sal14 = 25
+# 十五号服装
+time15 = "15号"
+name15 = "牛仔裤"
+pri15 = 86.3
+nmb15 = 600
+sal15 = 60
+# 十六号服装
+time16 = "16号"
+name16 = "T恤"
+pri16 = 65.8
+nmb16 = 632
+sal16 = 129
+# 十七号服装
+time17 = "17号"
+name17 = "羽绒服"
+pri17 = 253.6
+nmb17 = 500
+sal17 = 10
+# 十八号服装
+time18 = "18号"
+name18 = "风衣"
+pri18 = 96.8
+nmb18 = 335
+sal18 = 43
+# 十九号服装
+time19 = "19号"
+name19 = "T恤"
+pri19 = 65.8
+nmb19 = 632
+sal19 = 63
+# 二十号服装
+time20 = "20号"
+name20 = "牛仔裤"
+pri20 = 86.3
+nmb20 = 600
+sal20 = 60
+# 二十一号服装
+time21 = "21号"
+name21 = "皮草"
+pri21 = 135.9
+nmb21 = 855
+sal21 = 63
+# 二十二号服装
+time22 = "22号"
+name22 = "风衣"
+pri22 = 96.8
+nmb22 = 355
+sal22 = 60
+# 二十三号服装
+time23 = "23号"
+name23 = "T恤"
+pri23 = 65.8
+nmb23 = 632
+sal23 = 58
+# 二十四号服装
+time24 = "24号"
+name24 = "牛仔裤"
+pri24 = 86.3
+nmb24 = 600
+sal24 = 140
+# 二十五号服装
+time25 = "25号"
+name25 = "T恤"
+pri25 = 65.8
+nmb25 = 632
+sal25 = 48
+# 二十六号服装
+time26 = "26号"
+name26 = "风衣"
+pri26 = 96.8
+nmb26 = 335
+sal26 = 43
+# 二十七号服装
+time27 = "27号"
+name27 = "皮草"
+pri27 = 135.9
+nmb27 = 855
+sal27 = 57
+# 二十八号服装
+time28 = "28号"
+name28 = "羽绒服"
+pri28 = 253.6
+nmb28 = 500
+sal28 = 10
+# 二十九号服装
+time29 = "29号"
+name29 = "T恤"
+pri29 = 65.8
+nmb29 = 632
+sal29 = 63
+# 三十号服装
+time30 = "30号"
+name30 = "风衣"
+pri30 = 96.8
+nmb30 = 335
+sal30 = 78
+
+print("------------------------回忆服装------------------------")
+print("日期          商品名        价格       库存量       销售量")
+print(time1,"\t\t",name1,"\t\t",pri1,"\t\t",nmb1,"\t\t",sal1)
+print(time2,"\t\t",name2,"\t\t",pri2,"\t\t",nmb2,"\t\t",sal2)
+print(time3,"\t\t",name3,"\t\t",pri3,"\t\t",nmb3,"\t\t",sal3)
+print(time4,"\t\t",name4,"\t\t",pri4,"\t\t",nmb4,"\t\t",sal4)
+print(time5,"\t\t",name5,"\t\t",pri5,"\t\t",nmb5,"\t\t",sal5)
+print(time6,"\t\t",name6,"\t\t",pri6,"\t\t",nmb6,"\t\t",sal6)
+print(time7,"\t\t",name7,"\t\t",pri7,"\t\t",nmb7,"\t\t",sal7)
+print(time8,"\t\t",name8,"\t\t",pri8,"\t\t",nmb8,"\t\t",sal8)
+print(time9,"\t\t",name9,"\t\t",pri9,"\t\t",nmb9,"\t\t",sal9)
+print(time10,"\t\t",name10,"\t\t",pri10,"\t\t",nmb10,"\t\t",sal10)
+print(time11,"\t\t",name11,"\t\t",pri11,"\t\t",nmb11,"\t\t",sal11)
+print(time12,"\t\t",name12,"\t\t",pri12,"\t\t",nmb12,"\t\t",sal12)
+print(time13,"\t\t",name13,"\t\t",pri13,"\t\t",nmb13,"\t\t",sal13)
+print(time14,"\t\t",name14,"\t\t",pri14,"\t\t",nmb14,"\t\t",sal14)
+print(time15,"\t\t",name15,"\t\t",pri15,"\t\t",nmb15,"\t\t",sal15)
+print(time16,"\t\t",name16,"\t\t",pri16,"\t\t",nmb16,"\t\t",sal16)
+print(time17,"\t\t",name17,"\t\t",pri17,"\t\t",nmb17,"\t\t",sal17)
+print(time18,"\t\t",name18,"\t\t",pri18,"\t\t",nmb18,"\t\t",sal18)
+print(time19,"\t\t",name19,"\t\t",pri19,"\t\t",nmb19,"\t\t",sal19)
+print(time20,"\t\t",name20,"\t\t",pri20,"\t\t",nmb20,"\t\t",sal20)
+print(time21,"\t\t",name21,"\t\t",pri21,"\t\t",nmb21,"\t\t",sal21)
+print(time22,"\t\t",name22,"\t\t",pri22,"\t\t",nmb22,"\t\t",sal22)
+print(time23,"\t\t",name23,"\t\t",pri23,"\t\t",nmb23,"\t\t",sal23)
+print(time24,"\t\t",name24,"\t\t",pri24,"\t\t",nmb24,"\t\t",sal24)
+print(time25,"\t\t",name25,"\t\t",pri25,"\t\t",nmb25,"\t\t",sal25)
+print(time26,"\t\t",name26,"\t\t",pri26,"\t\t",nmb26,"\t\t",sal26)
+print(time27,"\t\t",name27,"\t\t",pri27,"\t\t",nmb27,"\t\t",sal27)
+print(time28,"\t\t",name28,"\t\t",pri28,"\t\t",nmb28,"\t\t",sal28)
+print(time29,"\t\t",name29,"\t\t",pri29,"\t\t",nmb29,"\t\t",sal29)
+print(time30,"\t\t",name30,"\t\t",pri30,"\t\t",nmb30,"\t\t",sal30)
+print("-------------------------------------------------------")
+print("商品名：",name1,"销售占比：",round(((sal1 + sal8 + sal10 + sal17 + sal28) / (sal1 + sal2 + sal3 + sal4 + sal5 + sal6 + sal7 + sal8 + sal9 + sal10 + sal11 + sal12 + sal13 + sal14 + sal15 + sal16 + sal17 + sal18 + sal19 + sal20 + sal21 + sal22 + sal23 + sal24 + sal25 + sal26 + sal27 + sal28 + sal29 + sal30)),2),"每月销售额占比：",round(((sal1 + sal8 + sal10 + sal17 + sal28) * pri1 / (sal1 * pri1 + sal2 * pri2 + sal3 * pri3 + sal4 * pri4 + sal5 * pri5 + sal6 * pri6 + sal7 * pri7 + sal8 * pri8 + sal9 * pri9 + sal10 * pri10 + sal11 * pri11 + sal12 * pri12 + sal13 * pri13 + sal14 * pri14 + sal15 * pri15 + sal16 * pri16 + sal17 * pri17 + sal18 * pri18 + sal19 * pri19 + sal20 * pri20 + sal21 * pri21 + sal22 * pri22 + sal23 * pri23 + sal24 * pri24 + sal25 * pri25 + sal26 * pri26 + sal27 * pri27 + sal28 * pri28 + sal29 * pri29 + sal30 * pri30)),2))
+print("商品名：",name2,"每件销售占比：",round(((sal2 + sal7 + sal9 + sal11 + sal15 + sal20 + sal24) / (sal1 + sal2 + sal3 + sal4 + sal5 + sal6 + sal7 + sal8 + sal9 + sal10 + sal11 + sal12 + sal13 + sal14 + sal15 + sal16 + sal17 + sal18 + sal19 + sal20 + sal21 + sal22 + sal23 + sal24 + sal25 + sal26 + sal27 + sal28 + sal29 + sal30)),2),"每月销售占比：",round(((sal2 + sal7 + sal9 + sal11 + sal15 + sal20 + sal24) * pri2 / (sal1 * pri1 + sal2 * pri2 + sal3 * pri3 + sal4 * pri4 + sal5 * pri5 + sal6 * pri6 + sal7 * pri7 + sal8 * pri8 + sal9 * pri9 + sal10 * pri10 + sal11 * pri11 + sal12 * pri12 + sal13 * pri13 + sal14 * pri14 + sal15 * pri15 + sal16 * pri16 + sal17 * pri17 + sal18 * pri18 + sal19 * pri19 + sal20 * pri20 + sal21 * pri21 + sal22 * pri22 + sal23 * pri23 + sal24 * pri24 + sal25 * pri25 + sal26 * pri26 + sal27 * pri27 + sal28 * pri28 + sal29 * pri29 + sal30 * pri30)),2))
+print("商品名：",name3,"每件销售占比：",round(((sal3 + sal14 + sal18 + sal22 + sal26 + sal30) / (sal1 + sal2 + sal3 + sal4 + sal5 + sal6 + sal7 + sal8 + sal9 + sal10 + sal11 + sal12 + sal13 + sal14 + sal15 + sal16 + sal17 + sal18 + sal19 + sal20 + sal21 + sal22 + sal23 + sal24 + sal25 + sal26 + sal27 + sal28 + sal29 + sal30)),2),"每月销售占比：",round(((sal3 + sal14 + sal18 + sal22 + sal26 + sal30) * pri3 / (sal1 * pri1 + sal2 * pri2 + sal3 * pri3 + sal4 * pri4 + sal5 * pri5 + sal6 * pri6 + sal7 * pri7 + sal8 * pri8 + sal9 * pri9 + sal10 * pri10 + sal11 * pri11 + sal12 * pri12 + sal13 * pri13 + sal14 * pri14 + sal15 * pri15 + sal16 * pri16 + sal17 * pri17 + sal18 * pri18 + sal19 * pri19 + sal20 * pri20 + sal21 * pri21 + sal22 * pri22 + sal23 * pri23 + sal24 * pri24 + sal25 * pri25 + sal26 * pri26 + sal27 * pri27 + sal28 * pri28 + sal29 * pri29 + sal30 * pri30)),2))
+print("商品名：",name4,"每件销售占比：",round(((sal4 + sal12 + sal21 + sal27) / (sal1 + sal2 + sal3 + sal4 + sal5 + sal6 + sal7 + sal8 + sal9 + sal10 + sal11 + sal12 + sal13 + sal14 + sal15 + sal16 + sal17 + sal18 + sal19 + sal20 + sal21 + sal22 + sal23 + sal24 + sal25 + sal26 + sal27 + sal28 + sal29 + sal30)),2),"每月销售占比：",round(((sal4 + sal12 + sal21 + sal27) * pri2 / (sal1 * pri1 + sal2 * pri2 + sal3 * pri3 + sal4 * pri4 + sal5 * pri5 + sal6 * pri6 + sal7 * pri7 + sal8 * pri8 + sal9 * pri9 + sal10 * pri10 + sal11 * pri11 + sal12 * pri12 + sal13 * pri13 + sal14 * pri14 + sal15 * pri15 + sal16 * pri16 + sal17 * pri17 + sal18 * pri18 + sal19 * pri19 + sal20 * pri20 + sal21 * pri21 + sal22 * pri22 + sal23 * pri23 + sal24 * pri24 + sal25 * pri25 + sal26 * pri26 + sal27 * pri27 + sal28 * pri28 + sal29 * pri29 + sal30 * pri30)),2))
+print("商品名：",name5,"每件销售占比：",round(((sal5 + sal13 + sal16 + sal19 + sal23 + sal25 + sal29) / (sal1 + sal2 + sal3 + sal4 + sal5 + sal6 + sal7 + sal8 + sal9 + sal10 + sal11 + sal12 + sal13 + sal14 + sal15 + sal16 + sal17 + sal18 + sal19 + sal20 + sal21 + sal22 + sal23 + sal24 + sal25 + sal26 + sal27 + sal28 + sal29 + sal30)),2),"每月销售占比：",round(((sal5 + sal13 + sal16 + sal19 + sal23 + sal25 + sal29) * pri5 / (sal1 * pri1 + sal2 * pri2 + sal3 * pri3 + sal4 * pri4 + sal5 * pri5 + sal6 * pri6 + sal7 * pri7 + sal8 * pri8 + sal9 * pri9 + sal10 * pri10 + sal11 * pri11 + sal12 * pri12 + sal13 * pri13 + sal14 * pri14 + sal15 * pri15 + sal16 * pri16 + sal17 * pri17 + sal18 * pri18 + sal19 * pri19 + sal20 * pri20 + sal21 * pri21 + sal22 * pri22 + sal23 * pri23 + sal24 * pri24 + sal25 * pri25 + sal26 * pri26 + sal27 * pri27 + sal28 * pri28 + sal29 * pri29 + sal30 * pri30)),2))
+print("商品名：",name6,"每件销售占比：",round((sal6 / (sal1 + sal2 + sal3 + sal4 + sal5 + sal6 + sal7 + sal8 + sal9 + sal10 + sal11 + sal12 + sal13 + sal14 + sal15 + sal16 + sal17 + sal18 + sal19 + sal20 + sal21 + sal22 + sal23 + sal24 + sal25 + sal26 + sal27 + sal28 + sal29 + sal30)),2),"每月销售占比：",round((sal6 * pri6 / (sal1 * pri1 + sal2 * pri2 + sal3 * pri3 + sal4 * pri4 + sal5 * pri5 + sal6 * pri6 + sal7 * pri7 + sal8 * pri8 + sal9 * pri9 + sal10 * pri10 + sal11 * pri11 + sal12 * pri12 + sal13 * pri13 + sal14 * pri14 + sal15 * pri15 + sal16 * pri16 + sal17 * pri17 + sal18 * pri18 + sal19 * pri19 + sal20 * pri20 + sal21 * pri21 + sal22 * pri22 + sal23 * pri23 + sal24 * pri24 + sal25 * pri25 + sal26 * pri26 + sal27 * pri27 + sal28 * pri28 + sal29 * pri29 + sal30 * pri30)),2))
+print("本月总销售额：",round((sal1 * pri1 + sal2 * pri2 + sal3 * pri3 + sal4 * pri4 + sal5 * pri5 + sal6 * pri6 + sal7 * pri7 + sal8 * pri8 + sal9 * pri9 + sal10 * pri10 + sal11 * pri11 + sal12 * pri12 + sal13 * pri13 + sal14 * pri14 + sal15 * pri15 + sal16 * pri16 + sal17 * pri17 + sal18 * pri18 + sal19 * pri19 + sal20 * pri20 + sal21 * pri21 + sal22 * pri22 + sal23 * pri23 + sal24 * pri24 + sal25 * pri25 + sal26 * pri26 + sal27 * pri27 + sal28 * pri28 + sal29 * pri29 + sal30 * pri30),2))
+
+
+
+
+
+
+

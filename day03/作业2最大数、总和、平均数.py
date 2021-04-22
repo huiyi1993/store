6d62d8984de260e497f60bc07e474c80c72d51e4
huiyi <178175002@qq.com>
2021-04-22 19:37:47 +0800

    第一次提交作业2最大数、总和、平均数.py

---
+++ day03/作业2最大数、总和、平均数.py
@@ -0,0 +1,29 @@
+num1 = input("请输入第一个数字：")
+num2 = input("请输入第二个数字：")
+num3 = input("请输入第三个数字：")
+num4 = input("请输入第四个数字：")
+num5 = input("请输入第五个数字：")
+num6 = input("请输入第六个数字：")
+num7 = input("请输入第七个数字：")
+num8 = input("请输入第八个数字：")
+num9 = input("请输入第九个数字：")
+num10 = input("请输入第十个数字：")
+num1 = int(num1)
+num2 = int(num2)
+num3 = int(num3)
+num4 = int(num4)
+num5 = int(num5)
+num6 = int(num6)
+num7 = int(num7)
+num8 = int(num8)
+num9 = int(num9)
+num10 = int(num10)
+print("最大值为：",max(num1,num2,num3,num4,num5,num6,num7,num8,num9,num10),
+      "和为：",sum((num1,num2,num3,num4,num5,num6,num7,num8,num9,num10)),
+      "平均值为：",sum((num1,num2,num3,num4,num5,num6,num7,num8,num9,num10)) / 10)
+
+
+
+
+
+
